5c0195d06f575114a39c8eb1b3b6b68e388b5bea
Gregor Heine <gheine@gilt.com>
2016-05-10 11:23:44 +0100

    Add extractors and matchers to match on nested Exceptions (#8)
    * More fun with nested Exceptions
    
    * Change extractor function to Matcher object

--- src/main/scala/com/gilt/gfc/util/Throwables.scala
+++ src/main/scala/com/gilt/gfc/util/Throwables.scala
@@ -3,28 +3,46 @@ package com.gilt.gfc.util
 import scala.annotation.tailrec
 
 object Throwables {
-  def rootCause(t: Throwable): Throwable = {
-    @tailrec def rootCause(t: Throwable, acc: Set[Throwable]): Throwable = {
-      val cause = Option(t.getCause).filterNot(acc.contains)
-      if (cause.isDefined) {
-        rootCause(cause.get, acc + t)
+  def unwind(t: Throwable): List[Throwable] = {
+    @tailrec def seq(t: Throwable, acc: List[Throwable]): List[Throwable] = {
+      val deduped = Option(t).filterNot(acc.contains)
+      if (deduped.isDefined) {
+        seq(t.getCause, t :: acc)
       } else {
-        t
+        acc
       }
     }
-    rootCause(t, Set.empty)
+    seq(t, Nil).reverse
   }
 
-  def messages(t: Throwable): Seq[String] = {
-    @tailrec def messages(t: Throwable, acc1: Set[Throwable], acc2: List[String]): Seq[String] = {
-      val msgs = Option(t.getMessage).fold(acc2)(acc2 ::)
-      val cause = Option(t.getCause).filterNot(acc1.contains)
-      if (cause.isDefined) {
-        messages(cause.get, acc1 + t, msgs)
-      } else {
-        msgs
+  def rootCause(t: Throwable): Throwable = {
+    if (t == null) throw new NullPointerException else unwind(t).last
+  }
+
+  def messages(t: Throwable): List[String] = {
+    unwind(t).map(_.getMessage).filterNot(_ == null).distinct
+  }
+
+  def isA[T <: Throwable](clazz: Class[T])(t: Throwable): Option[T] = {
+    unwind(t).find(t => clazz.isAssignableFrom(t.getClass)).map(_.asInstanceOf[T])
+  }
+
+  def apply[X](pf: PartialFunction[Throwable, X]): PartialFunction[Throwable, X] = {
+    new PartialFunction[Throwable, X] {
+      override def isDefinedAt(t: Throwable): Boolean = unwind(t).exists(pf.isDefinedAt)
+      override def apply(t: Throwable): X = unwind(t).collectFirst {
+        case t if pf.isDefinedAt(t) => pf.apply(t)
+      } match {
+        case Some(x) => x
       }
     }
-    messages(t, Set.empty, Nil).reverse.distinct
+  }
+
+  object Matcher {
+    def apply[T <: Throwable](clz: Class[T]): { def unapply(t: Throwable): Option[T] } = {
+      new {
+        def unapply(t: Throwable): Option[T] = isA(clz)(t)
+      }
+    }
   }
 }
--- src/test/scala/com/gilt/gfc/util/ThrowablesTest.scala
+++ src/test/scala/com/gilt/gfc/util/ThrowablesTest.scala
@@ -1,8 +1,16 @@
 package com.gilt.gfc.util
 
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
 import org.scalatest.{Matchers, FunSuite}
 
 class ThrowablesTest extends FunSuite with Matchers {
+  test("null throws NPE") {
+    a[NullPointerException] shouldBe thrownBy(Throwables.rootCause(null))
+  }
+
   test("Linear root cause") {
     val root = new RuntimeException
     val top = (1 to 3).foldLeft(root)((exc, _) => new RuntimeException(exc))
@@ -54,5 +62,88 @@ class ThrowablesTest extends FunSuite with Matchers {
     val two = new RuntimeException("bar", one)
     val three = new RuntimeException("foo", two)
     Throwables.messages(three) shouldBe Seq("foo", "bar")
+  }
+
+  test("Is a RuntimeException") {
+    val rte = new RuntimeException("foo", new Exception)
+    val isAnRTE = Throwables.isA(classOf[RuntimeException]) _
+
+    isAnRTE(rte) shouldBe Some(rte)
+    isAnRTE(new Exception(rte)) shouldBe Some(rte)
+    isAnRTE(new Exception(new Exception)) shouldBe None
+
+    (new Exception("bang", rte) match {
+      case ex if isAnRTE(ex).isDefined => ex.getMessage
+    }) shouldBe "bang"
+  }
+
+  test("pattern matching nested exceptions") {
+    val nestedRte = new Exception(new RuntimeException("boom"))
+    val noRte = new Exception(new Exception())
+
+    {
+      try(throw nestedRte)
+      catch Throwables {
+        case rte: RuntimeException => rte.getMessage
+      }
+    } shouldBe "boom"
+
+    an[Exception] shouldBe thrownBy {
+      try(throw noRte)
+      catch Throwables {
+        case rte: RuntimeException => rte.getMessage
+      }
+    }
+
+    Throwables {
+      case rte: RuntimeException => rte.getMessage
+    }{
+      nestedRte
+    } shouldBe "boom"
+
+    an[Exception] shouldBe thrownBy {
+      Throwables {
+        case rte: RuntimeException => rte.getMessage
+      }(noRte)
+    }
+
+    Try(throw nestedRte).recover(Throwables {
+      case rte: RuntimeException => rte.getMessage
+    }) shouldBe Success("boom")
+
+    Try(throw noRte).recover(Throwables {
+      case rte: RuntimeException => rte.getMessage
+    }) shouldBe Failure(noRte)
+  }
+
+  test("extracting nested exceptions") {
+    val nestedRte = new Exception(new RuntimeException("boom"))
+    val noRte = new Exception(new Exception())
+
+    val RTE = Throwables.Matcher(classOf[RuntimeException])
+
+    {
+      nestedRte match {
+        case RTE(e) => e.getMessage
+      }
+    } shouldBe "boom"
+
+    {
+      nestedRte match {
+        case RTE(e) if e.getMessage == "boom" => e.getMessage
+      }
+    } shouldBe "boom"
+
+    an[Exception] shouldBe thrownBy {
+      nestedRte match {
+        case RTE(e) if e.getMessage == "bang" => e.getMessage
+      }
+    }
+
+    an[Exception] shouldBe thrownBy {
+      noRte match {
+        case RTE(e) => e.getMessage
+      }
+    }
   }
 }
